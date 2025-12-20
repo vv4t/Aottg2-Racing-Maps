a4dbaa441edecfec2df4c046fd46b70648a4b71f
vv4t <watashidesu6969@gmail.com>
2025-12-20 21:25:03 +1100

    separate folders cuz github complaining

--- image_check.sh
+++ image_check.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 for file in maps/*.txt; do
-  if [ ! -f "maps/$(basename "$file" .txt).jpg" ]; then
-    echo "Missing image for '$file'"
-    echo "Expected maps/$(basename "$file" .txt).jpg'"
+  image_file="images/$(basename "$file" .txt).jpg"
+  if [ ! -f "$image_file" ]; then
+    echo "Missing image '$image_file'"
+  fi
+done
+
+echo "---------------------------------------------"
+
+for file in images/*.jpg; do
+  text_file="maps/$(basename "$file" .jpg).txt"
+  if [ ! -f "$text_file" ]; then
+    echo "Image exists for missing map '$text_file'"
   fi
 done
